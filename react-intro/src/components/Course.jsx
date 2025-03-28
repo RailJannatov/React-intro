4ebce463d116c442babff28d7c6a972f5e6684fe
RailJannatov <railcennetov@gmail.com>
2025-03-28 20:42:39 +0400

    add test

--- react-intro/src/components/Course.jsx
+++ react-intro/src/components/Course.jsx
@@ -1,9 +1,18 @@
 export function Course({ name, address, src, alt }) {
   return (
     <div>
+
+      
       <p>{name}   ss  s </p>
+
+      <p>teetsf</p>
       <p>{address}</p>
       <div>
+
+
+
+
+        
         <img src={src} alt={`${alt}`} />
       </div>
       <div>
